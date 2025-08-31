f1dcdf2e5fc87b39bfdf7f9a55f0de76dc099fe6
Umer Farooque <mohranvi4484@gmail.com>
2025-08-31 21:02:32 +0500

    created objects

--- app.js
+++ app.js
@@ -1,21 +1,92 @@
 var mobile = {
     iphone: {
-
+        iPhone11: {
+            name: "iPhone 11",
+            released: "September 20, 2019",
+            display: 6.1,
+            camera: "12 MP",
+            storage: "64GB",
+            battery: "3110 mAh",
+            iOS: "iOS 13 up to 18.6",
+            weight: "161 g",
+            price: "270$"
+        },
+        iPhone12: {
+            name: "iPhone 12",
+            released: "October 23, 2020",
+            display: 6.1,
+            camera: "12 MP",
+            storage: "64 GB",
+            battery: "2815 mAh",
+            iOS: "iOS 13 up to 18.6",
+            weight: "164 g",
+            price: "300$"
+        },
     },
     samsung: {
-
+        samsungA56: {
+            name: "samsung A56",
+            display: 6.7,
+            camera: "50 MP",
+            storage: "256 GB",
+            battery: "5000 mAh",
+            weight: "198 g",
+            price: "450$"
+        }
     },
     oppo:{
-
+        oppoF16: {
+            name: "Oppo F16",
+            display: 6.7,
+            camera: "50 MP",
+            storage: "256 GB",
+            battery: "5000 mAh",
+            weight: "198 g",
+            price: "120$"
+        }
     },
     vivo: {
-
+        vivoY20: {
+            name: "Vivo Y20",
+            display: 6.7,
+            camera: "50 MP",
+            storage: "256 GB",
+            battery: "5000 mAh",
+            weight: "198 g",
+            price: "120$"
+        }
     },
     redme: {
-
+        redme13: {
+            name: "Redme 13",
+            display: 6.7,
+            camera: "50 MP",
+            storage: "128 GB",
+            battery: "5000 mAh",
+            weight: "198 g",
+            price: "100$"
+        }
     },
     infinix: {
-
+        infinixHot8: {
+            name: "Infinix Hot8",
+            display: 6.7,
+            camera: "13 MP",
+            storage: "64 GB",
+            battery: "5000 mAh",
+            weight: "198 g",
+            price: "80$"
+        }
     },
-    
+    Nokia: {
+        nokiaTurboPro: {
+            name: "Nokia Turbo Pro",
+            display: 6.7,
+            camera: "144 MP",
+            storage: "512 GB",
+            battery: "7500 mAh",
+            weight: "198 g",
+            price: "500$"
+        }
+    }
 }
